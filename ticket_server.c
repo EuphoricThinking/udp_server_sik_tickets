3d8aca0598763f9c66250c21f86dd3c9c93da886
Agata <amomot@bio-prodict.nl>
2022-04-10 20:29:56 +0200

    Written getopt in ticket_server, not tested

--- ticket_server.c
+++ ticket_server.c
@@ -1,12 +1,28 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <ctype.h>
+#include <stdbool.h>
 
 #define OPTSTRING ":fpt"
 #define PORT_MIN 0
 #define PORT_MAX 65535
 #define TIME_MIN 1
 #define TIME_MAX 86400
+
+#define PORT_DEF 2022
+#define TIME_DEF 5
+
+bool is_number(char* arg_opt) {
+	int index = 0;
+	while (arg_opt[index] != '\0') {
+		if (!isdigit(arg_opt[index])) {
+
+			return false;
+		}
+	}
+
+	return true;
+}
 
 char* read_options(int argc, char* argv, int* port_number, int* timeout) {
 	int option;
@@ -17,27 +33,48 @@ char* read_options(int argc, char* argv, int* port_number, int* timeout) {
 		case 'f':
 			filename = optarg;
 		case 'p':
-			if (!isdigit(optarg=
+			if (!is_number(optarg)) {
+				printf("Port number should be passed as a"
+					" numeric code\n">
+                                exit(1);
+                        }
+
 			*port_number = atoi(optarg);
 			if (port_number < PORT_MIN || port_number > PORT_MAX) {
 				printf("Port number should be included in "
 					"range [%d, %d]\n", PORT_MIN, PORT_MAX);
 				exit(1);
 			}
+
 		case 't':
-			if (!isdigit(optarg[0])) {
-				printf("Timeout should be passed as a number\n");
+			if (!is_number(optarg)) {
+				printf("Timeout should be passed as a "
+					"numeric code\n");
 				exit(1);
 			}
 
 			*timeout = atoi(optarg);
-			if (timeout == 1) {
-				printf("Timeout should be passed as a number\n">
-                                exit(1);
-                        }
-
 			if (timeout < TIME_MIN || timeout > TIME_MAX) {
 				printf("Timeout should be a number specified"
 					" in range [%d, %d]\n", TIME_MIN,
 					TIME_MAX);
 				exit(1);
+
+		default: /* ? */
+			printf("Unrecognized argument\n");
+			exit(1);
+		}
+	}
+
+	return filename;
+}
+
+
+int main(int argc, char* argv) {
+	int timeout = TIME_DEF;
+	int port = PORT_DEF;
+
+	char* filename = read_options(argc, argv, &port, &timeout);
+
+	return 0;
+}
