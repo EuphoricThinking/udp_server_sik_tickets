5a790642a513b412a43308d3c7d6d360f1d91cf9
Agata <amomot@bio-prodict.nl>
2022-04-12 23:38:40 +0200

    Conversion works, but I need to work on queue

--- ticket_server.c
+++ ticket_server.c
@@ -85,6 +85,116 @@ typedef struct Client_message {
     char* cookie;
 } Client_message;
 
+
+/*
+ * Queue
+ */
+
+typedef struct List {
+    uint64_t val;
+
+    struct List* next;
+} List;
+
+typedef struct Queue {
+    List* first;
+    List* last;
+
+    int num_elements;
+} Queue;
+
+List* init_node(uint64_t val) {
+    List* l = malloc(sizeof(List));
+
+    l->next = NULL;
+    l->val = val;
+
+    return l;
+}
+
+void delete_node(List* l) {
+    l->next = NULL;
+    free(l);
+}
+
+void delete_nodes_all(List* l) {
+    if (l) {
+        List *child = l->next;
+        List *current = l;
+
+        while (current) {
+            delete_node(current);
+            current = child;
+            if (child) {
+                child = current->next;
+            }
+        }
+    }
+}
+
+Queue* init_queue() {
+    Queue* q = malloc(sizeof(Queue));
+    q->first = NULL;
+    q->last = NULL;
+    q->num_elements = 0;
+
+    return q;
+}
+
+void push(Queue* q, uint64_t to_insert_val) {
+    if (q->num_elements == 0) {
+        q->first = init_node(to_insert_val);
+        q->last = q->first;
+        q->num_elements++;
+    }
+    else {
+        List* to_append = init_node(to_insert_val);
+        q->last->next = to_append;
+        q->last = to_append;
+        q->num_elements++;
+    }
+}
+
+List* top(Queue* q) {
+    if (q->num_elements == 0) {
+        return NULL;
+    }
+    else {
+        return q->first;
+    }
+}
+
+List* pop(Queue* q) {
+    if (q->num_elements > 0) {
+        List *to_return = q->first;
+        List *second = q->first->next;
+        q->first = second;
+        q->num_elements--;
+
+        return to_return;
+    }
+    else {
+        return NULL;
+    }
+}
+
+void delete_queue(Queue* q) {
+    delete_nodes_all(q->first);
+    free(q);
+}
+
+bool is_empty(Queue* q) {
+    return q->num_elements == 0;
+}
+
+
+/*
+ * End of queu
+ */
+
+
+
+
 bool is_number(char* arg_opt) {
 	int index = arg_opt[0] == '-' ? 1 : 0;
 
@@ -342,9 +452,10 @@ void print_client_message(Client_message clm, bool if_cookies) {
 uint32_t bitshift_to_retrieve_message(int begining, int end, char* message) {
     uint32_t result = 0;
     for (int i = begining; i < end; i++) {
+        printf("ind %d BYTE %d 32 %d \n", i, message[i], (uint32_t) message[i]);
         result |= ((uint32_t) message[i] << 8*(i - begining));
     }
-
+    printf("end\n");
     return result;
 }
 
@@ -456,6 +567,9 @@ Client_message interpret_client_message(char* message, size_t received_length,
 //            void* ptr = message + 1;
 //            uint32_t converted = *((uint32_t*) ptr);
 //            printf("CAST %d\n", converted);
+            char* ptr = message + 1;
+            uint32_t converted = *(uint32_t*)ptr;
+            printf("CONVERTED %d\n", converted);
 
             if (event_id > (events.len - 1)) {
                 result_message.ticket_count = ERR_EVENTS_TICK;
